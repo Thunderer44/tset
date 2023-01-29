d39458e025f0ab639915f921f5fd7ef7b3648a13
Thunderer44 <Thunderer44@users.noreply.github.com>
2023-01-29 18:33:12 +0530

    Color changes

---
+++ net_banking/contact.php
@@ -0,0 +1,67 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact_style.css">
+</head>
+
+<body>
+    <div class="flex-container-background">
+        <div class="flex-container-heading">
+            <h1 id="contact">Contact Us</h1>
+        </div>
+
+        <div class="flex-container" style="border-bottom: 0;
+                                           border-top-left-radius: 10px;
+                                           border-top-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Corporate Headquarters</h1>
+                <p id="sub-contact">
+                    Corporate HQ<br>Project Bank<br>
+                    Patparganj Rd, Madhuban colony,<br>
+		    Nirman Vihar, <br>
+		    Shakarpur,New Delhi, Delhi 110092<br>
+                    
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">General Contact</h1>
+                <p id="sub-contact">
+                    Toll-Free: 123-456-7890<br>
+                    Phone: 123-456-7890<br>
+                    Fax: 123-456-7890<br>
+                    Email: office@projectbank.com
+                </p>
+            </div>
+        </div>
+
+        <div class="flex-container" style="border-top: 0;
+                                           border-bottom-left-radius: 10px;
+                                           border-bottom-right-radius: 10px;">
+            <div class="flex-item">
+                <h1 id="sub-contact">Customer Care (24x7)</h1>
+                <p id="sub-contact">
+                    Toll-Free: 123-456-7891<br>
+                    Phone: 123-456-7891<br>
+                    Email: care@projectbank.com
+                </p>
+            </div>
+            <div class="flex-item">
+                <h1 id="sub-contact">Live Chat</h1>
+                <p id="sub-contact">
+                    Download our app and live chat<br>
+                    with our customer care !<br>
+                    App available on Google Play<br>
+                    and iPhone-AppStore.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
